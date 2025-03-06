5fba7c601e64311fc96a368f43f26c71f5b83272
Rex Liu <lzhen@LAPTOP-RM595307>
2025-03-06 10:52:08 +0800

    Add project files.

---
+++ ExcelTest.Aspose/TestAspose.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using NUnit.Framework;
+using Aspose;
+using Aspose.Cells;
+
+namespace ExcelTest.Aspose
+{
+    [TestFixture]
+    public class TestAspose
+    {
+        [Test]
+        public void TestDeleteRow()
+        {
+            var wb = GetTestWorkbook();
+
+            var sheet1 = wb.Worksheets[0];
+            sheet1.Cells.DeleteRow(0);
+
+            wb.Save(@"c:\temp\testSheet_deleteOneRow.xlsx");
+        }
+
+        [Test]
+        public void TestWorkbookCreation()
+        {
+            var wb = GetTestWorkbook();
+            wb.Save(@"c:\temp\testSheet.xlsx");
+        }
+
+        public Workbook GetTestWorkbook()
+        {
+            // For complete examples and data files, please go to https://github.com/aspose-cells/Aspose.Cells-for-.NET
+            // Create workbook
+            Workbook wb = new Workbook();
+
+            // add some data to the first sheet
+            Worksheet sht1 = wb.Worksheets[0];
+            for (int i = 1; i<=100; i++)
+            {
+                sht1.Cells[$"A{i}"].PutValue($"Value {i}");
+
+                sht1.Cells[$"C{i}"].PutValue(i);
+
+                sht1.Cells[$"E{i+10}"].PutValue(i+10);
+            }
+
+            // create formula
+            Cell b15 = sht1.Cells["B15"];
+            b15.Formula = "=C15*E15";
+            CellsFactory f = new CellsFactory();
+            Style s = f.CreateStyle();     
+            s.BackgroundColor = Color.Red;
+            s.Font.IsBold = true;
+            s.ForegroundColor = Color.Yellow;
+            b15.SetStyle(s);
+
+            // create some named range:
+            Range range = sht1.Cells.CreateRange("B20", "E20");
+            range.Name = "Test_Range"; //Setting the name of the named range           
+
+            // set calculatioin mode
+            wb.Settings.CalcMode = CalcModeType.Automatic;
+
+            return wb;
+        }
+    }
+}
---
+++ ExcelTest.DX/TestDXSpreadsheet.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using NUnit.Framework;
+using DevExpress.Spreadsheet;
+
+
+namespace ExcelTest.DX
+{
+    [TestFixture]
+    public class TestDXSpreadsheet
+    {
+        [Test]
+        public void TestDeleteRow()
+        {
+            var wb = GetTestWorkbook();
+
+            var sheet1 = wb.Worksheets[0];
+            sheet1.Rows[0].Delete();
+
+            wb.SaveDocument(@"c:\temp\testSheet_DX_deleteOneRow.xlsx");
+        }
+
+        [Test]
+        public void TestWorkbookCreation()
+        {
+            var wb = GetTestWorkbook();
+            wb.SaveDocument(@"c:\temp\testSheet_DX.xlsx");
+        }
+        public Workbook GetTestWorkbook()
+        {
+            // For complete examples and data files, please go to https://github.com/aspose-cells/Aspose.Cells-for-.NET
+            // Create workbook
+            Workbook wb = new Workbook();
+            wb.Unit = DevExpress.Office.DocumentUnit.Point;
+
+            // add some data to the first sheet
+            Worksheet sht1 = wb.Worksheets[0];
+
+            wb.BeginUpdate();
+            try
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    sht1.Cells[$"A{i}"].SetValue($"Value {i}");
+
+                    sht1.Cells[$"C{i}"].SetValue(i);
+
+                    sht1.Cells[$"E{i + 10}"].SetValue(i + 10);
+                }
+
+                // create formula
+                Cell b15 = sht1.Cells["B15"];
+                b15.Formula = "=C15*E15";
+                b15.Style.BeginUpdate();
+                b15.FillColor = Color.Red;
+                b15.Font.Color = Color.White;
+
+                // create some named range:
+                CellRange rangeB20E20 = sht1.Range["B20:E20"];
+                rangeB20E20.Name = "Test_Range"; //Setting the name of the named range     
+
+                // set calculation mode!!
+                wb.DocumentSettings.Calculation.Mode = CalculationMode.Automatic;
+            }
+            finally
+            {
+                wb.EndUpdate();
+            }
+
+            wb.CalculateFull();
+                  
+            return wb;
+        }
+
+    }
+}
